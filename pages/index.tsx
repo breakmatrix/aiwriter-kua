ef66bb4c162f20ff80560cf85c46ad521315c396
breakmatrix <breakmatrix@gmail.com>
2023-06-13 16:08:57 +0800

    add head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Head from "next/head";
 import CardGrid from "../components/CardGrid";
 import {TEMPLATES} from "../constants/templates";
 
@@ -7,6 +8,10 @@ const HomePage: React.FC = () => {
 
     return (
         <Layout title="">
+            <Head>
+                <title>跨境电商 ChatGPT 提示词生成器</title>      
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <h1 className="text-4xl font-bold mt-10 ml-10">
                 欢迎来到 <span className="text-blue-600">跨境电商 ChatGPT 提示词生成器</span>
                 
